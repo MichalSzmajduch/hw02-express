569246655ca5a96285e0ab3cae5e0c9c919da086
MichalSzmajduch <m.szmajduch1996@gmail.com>
2024-01-25 21:19:46 +0100

    hw3 uncomplited

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
 
-const contactsRouter = require('./routes/api/contacts')
+import { router as contactRoutes } from "./routes/api/contacts.routes.js";
 
-const app = express()
+import dotenv from "dotenv";
+dotenv.config();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const PORT = process.env.PORT || 4000;
 
-app.use('/api/contacts', contactsRouter)
+const connection = mongoose.connect(process.env.DB_URL, {
+  dbName: "db-contacts",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+app.use(express.json());
+app.use(cors());
+app.use("/api", contactRoutes);
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+connection
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`App listens on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Error while establishing connection: [${error}]`);
+    process.exit(1);
+  });
 
-module.exports = app
+export default { app };
